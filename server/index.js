867a703d6e0180f686b23eb0aaf674871b5a740a
IlyaLitvin <darnon88@gmail.com>
2021-04-23 18:40:54 +0300

    some

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const sequelize = require("./db");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const app = express();
 const cors = require("cors");
 const logger = require("morgan");
