3d9c7e9dfe843fafed5dff90d3d5398fa7d3a3b5
IlyaLitvin <darnon88@gmail.com>
2021-05-05 01:35:20 +0300

    d

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,7 +5,7 @@ const { User, Profile } = require("../models/models");
 // const { func, function } = require("joi");
 
 const generateJwt = (id) => {
-  return jwt.sign({ id }, "~E(/]s@}};8a%|/s)ni5z_Ji+B");
+  return jwt.sign({ id }, "~E(/]s@}};8a%|/s)ni5z_Ji+B", { expiresIn: "24h" });
 };
 
 async function registration(req, res) {
